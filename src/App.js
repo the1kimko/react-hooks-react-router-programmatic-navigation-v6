3da3ed3cb5362e74b8691e1ec5d1b7a9f4b6f1ed
KomfortKimko <komfortkimk@gmail.com>
2024-08-08 07:17:02 +0300

    React-router-programmatic-navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
+  const login = () =>{
+    setIsLoggedIn(true);
+  }
+
+  const logout = () =>{
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() =>{
+    if (isLoggedIn) {
+        // navigates to Home route if user is logged in
+      navigate("/");
+    } else {
+        // navigates to Login route if user is logged out
+      navigate("/login");
+    };
+  }, [isLoggedIn, navigate]);
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      <NavBar logout={isLoggedIn ? <NavBar logout={logout}  /> : <Navigate to="/login" />} />
+      <Outlet context={login}/>
     </div>
   );
 }
